c324ac197c3f3443b042909c7fb1cb46c94a5b9c
Noah Wolfe <Noah Wolfe>
2016-07-22 19:38:50 -0400

    Added JSON parsing using javax.json

--- src/main/java/com/wolfe/app/webapis/fetch/FetchData.java
+++ src/main/java/com/wolfe/app/webapis/fetch/FetchData.java
@@ -2,12 +2,12 @@ package webapis.fetch;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URL;
 import java.net.MalformedURLException;
 
-//"http://apps.who.int/gho/athena/data/GHO/WHS3_48.json?profile=simple&filter=COUNTRY:*"
+import javax.json.*;
 
 public class FetchData
 {
@@ -20,20 +20,31 @@ public class FetchData
     {
         URL url;
         InputStream is = null;
-        BufferedReader br;
         String line;
         String data = "";
 
         try {
             url = new URL(link);
             is = url.openStream();
-            br = new BufferedReader(new InputStreamReader(is));
 
-            while ((line = br.readLine()) != null) {
+            JsonReader rdr = Json.createReader(is);
+            JsonObject obj = rdr.readObject();
+            JsonArray results = obj.getJsonArray("fact");
+
+            for (JsonObject result : results.getValuesAs(JsonObject.class)) {
+                System.out.println(result.getJsonObject("dim").getString("COUNTRY"));
+                System.out.println(": ");
+                System.out.println(result.getString("Value"));
+                System.out.println("----------");
+            }
+
+            //br = new BufferedReader(new InputStreamReader(is));
+
+            /*while ((line = br.readLine()) != null) {
                 line = line.replace("\n", ""); //get rid of all the newlines - make parsing a tiny bit faster
                 line = line.replace(" ", ""); //get rid of spaces
                 data += line;
-            }
+            }*/
         } catch (MalformedURLException mue) {
              mue.printStackTrace();
         } catch (IOException ioe) {
