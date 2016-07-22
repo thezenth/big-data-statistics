09f8e4ded07b29bcdca18a63029b26e31bb88eee
Noah Wolfe <Noah Wolfe>
2016-07-21 22:51:19 -0400

    Working ability to get from url using java.net.URL

--- src/main/java/com/wolfe/app/Main.java
+++ src/main/java/com/wolfe/app/Main.java
@@ -1,5 +1,13 @@
 package com.wolfe.app;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.MalformedURLException;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,33 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        URL url;
+        InputStream is = null;
+        BufferedReader br;
+        String line;
+
+        try {
+            url = new URL("http://apps.who.int/gho/athena/data/GHO/WHS3_48.json?profile=simple&filter=COUNTRY:*");
+            is = url.openStream(); //throws an IOException - we haven't yet told the computer how to handle the IO
+            br = new BufferedReader(new InputStreamReader(is));
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (MalformedURLException mue) { //URL is somehow wrong - like if it started with hhtp:// instead of http://
+             mue.printStackTrace();
+        } catch (IOException ioe) { //IO = input/output, so generally something is wrong with the server's mechanism to actually give us data
+            ioe.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
+
     }
 }
